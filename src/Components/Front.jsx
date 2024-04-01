01397ad1591f93931f312c449dc09b3640110520
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-04-01 13:46:51 +0530

    Completes Front Part

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -3,10 +3,13 @@ import './Front.css';
 
 const Front = () => {
 	return (
-		<div className='w-[350px] rounded-bl-xl gap-10 flex flex-col items-end      h-[500px] bg-[#DC0433]'>
-			<div className='top2 h-[114px]  rounded-tl-xl flex justify-between items-top bg-[#840413]'>
-				<div className='top w-[360px]  rounded-tl-xl h-[100px] bg-[#DC0433]'></div>
+		<div className='w-[350px] rounded-bl-xl gap-5 flex flex-col shadow-2xl  shadow-black  h-[550px] bg-gradient-to-br  from-40% from-[#a60729]  to-[#DC0433] '>
+			<div className='flex flex-col items-end'>
+				<div className='top2 h-[114px]  rounded-tl-xl flex justify-between items-top bg-[#840413]'>
+					<div className='top w-[360px]  rounded-tl-xl h-[100px] bg-[#DC0433]'></div>
+				</div>
 			</div>
+
 			<div className='w-full '>
 				<div className='window-wrap'>
 					<div className='w-[76%] flex flex-col justify-center gap-3  items-center rounded-lg window m-auto h-[200px] bg-white'>
@@ -22,8 +25,97 @@ const Front = () => {
 								<div className='h-[2px] w-10 bg-black'></div>
 								<div className='h-[2px] w-10 bg-black'></div>
 								<div className='h-[2px] w-10 bg-black'></div>
-
 							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='flex flex-col gap-2'>
+				<div className='flex justify-center gap-5 items-start'>
+					<button className='btn  rounded-sm  min-w-[75px]  cursor-pointer bg-[#5d0303]'>
+						<div className='text-white inner font-semibold border-l-[1px] border-t-[1px] border-white   -translate-x-[1.5px] -translate-y-[1.5px] rounded-sm px-3  bg-red-600 '>
+							START
+						</div>
+					</button>
+
+					<button className='btn min-w-[75px]   rounded-sm cursor-pointer bg-[#5d0303]'>
+						<div className='text-white inner shadow-inner font-semibold  -translate-x-[1.5px] -translate-y-[1.5px] rounded-sm px-3   bg-red-600 border-l-[1px] border-t-[1px] border-white  '>
+							STOP
+						</div>
+					</button>
+				</div>
+				<button className=' ml-10 btn rounded-full w-10 h-10 cursor-pointer bg-[#5d0303]'>
+					<div className='font-bold text-white inner w-10 h-10 flex items-center justify-center   -translate-x-[1.5px] -translate-y-[1.5px] rounded-full bg-gradient-to-br from-white to-25%  to-blue-900  border-l-[1px] border-t-[1px] border-white  '>
+						A
+					</div>
+				</button>
+				<div className='flex justify-around px-5 items-center'>
+					<div className='bg-black w-[130px] min-w-[130px] h-14'></div>
+					<div className='bg-gradient-to-br pl-[3px] pt-[3px]	 border-r-4 flex flex-col justify-center items-center to-80% to-red-700  border-b-2 border-red-700  from-[#4b0505] shadow-[10px_15px_10px_-15px_rgba(0,0,0,0.6)] rounded-full w-24 h-24'>
+						<div className='flex'>
+							<button className='wrap-btn w-6 h-6 bg-[#301f1f] rounded-sm '>
+								<div className='w-6 h-6 buttons -translate-x-[1px] -translate-y-[1px] bg-black flex justify-center items-center'>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										width='32'
+										height='32'
+										viewBox='0 0 32 32'
+										id='dropup-arrow'
+										className='text-white w-full h-full  fill-current'
+									>
+										<path d='M8.998 20h13.966c.286 0 .54-.138.72-.352l.004.004c.39-.434.39-1.138 0-1.572l-6.998-7.754a.926.926 0 0 0-1.41 0c-.01.012-.014.028-.024.04L8.272 18.08a1.202 1.202 0 0 0 0 1.572l.012.01a.945.945 0 0 0 .714.338z'></path>
+									</svg>
+								</div>
+							</button>
+						</div>
+						<div className='flex'>
+							<button className='wrap-btn w-6 h-6 bg-[#301f1f] rounded-sm '>
+								<div className='w-6 h-6 buttons -translate-x-[1px] -translate-y-[1px] bg-black flex justify-center items-center'>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										width='32'
+										height='32'
+										viewBox='0 0 32 32'
+										id='dropup-arrow'
+										className='text-white w-full h-full -rotate-90 fill-current'
+									>
+										<path d='M8.998 20h13.966c.286 0 .54-.138.72-.352l.004.004c.39-.434.39-1.138 0-1.572l-6.998-7.754a.926.926 0 0 0-1.41 0c-.01.012-.014.028-.024.04L8.272 18.08a1.202 1.202 0 0 0 0 1.572l.012.01a.945.945 0 0 0 .714.338z'></path>
+									</svg>
+								</div>
+							</button>
+							<button className='wrap-btn w-6 h-6 bg-[#301f1f] rounded-sm '>
+								<div className='w-6 h-6 buttons border-0 -translate-x-[1px] -translate-y-[1px] bg-black flex justify-center items-center'></div>
+							</button>
+							<button className='wrap-btn w-6 h-6 bg-[#301f1f] rounded-sm '>
+								<div className='w-6 h-6 buttons -translate-x-[1px] -translate-y-[1px] bg-black flex justify-center items-center'>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										width='32'
+										height='32'
+										viewBox='0 0 32 32'
+										id='dropup-arrow'
+										className='text-white w-full h-full rotate-90  fill-current'
+									>
+										<path d='M8.998 20h13.966c.286 0 .54-.138.72-.352l.004.004c.39-.434.39-1.138 0-1.572l-6.998-7.754a.926.926 0 0 0-1.41 0c-.01.012-.014.028-.024.04L8.272 18.08a1.202 1.202 0 0 0 0 1.572l.012.01a.945.945 0 0 0 .714.338z'></path>
+									</svg>
+								</div>
+							</button>
+						</div>
+						<div className='flex'>
+							<button className='wrap-btn w-6 h-6 bg-[#301f1f] rounded-sm '>
+								<div className='w-6 h-6 buttons -translate-x-[1px] -translate-y-[1px] bg-black flex justify-center items-center'>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										width='32'
+										height='32'
+										viewBox='0 0 32 32'
+										id='dropup-arrow'
+										className='text-white w-full h-full rotate-180 fill-current'
+									>
+										<path d='M8.998 20h13.966c.286 0 .54-.138.72-.352l.004.004c.39-.434.39-1.138 0-1.572l-6.998-7.754a.926.926 0 0 0-1.41 0c-.01.012-.014.028-.024.04L8.272 18.08a1.202 1.202 0 0 0 0 1.572l.012.01a.945.945 0 0 0 .714.338z'></path>
+									</svg>
+								</div>
+							</button>
 						</div>
 					</div>
 				</div>
